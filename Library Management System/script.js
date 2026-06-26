62ace17060aa52cc324b753fa6c04597f09a5fd6
shrutidhanani59-sketch <shrutidhanani59@gmail.com>
2026-06-26 17:57:02 +0530

    Update script.js

--- Library Management System/script.js
+++ Library Management System/script.js
@@ -22,63 +22,79 @@ document.querySelector('.Display').onclick = function () {
 
 document.querySelector('.issue').onclick = function () {
     var name = prompt("Enter the book Name : ");
+    var bool = false
     for (const element of books) {
-        if (element.BookName === name) {
+        if (element.BookName === name) 
+        {
+            if(element.Quantity > 0)
+            {
+                element.Quantity--;
             document.querySelector('div').innerHTML += `<h2>Book ID : ${element.BookId}</h2>  <h2>Book Name :  ${element.BookName}</h2> <h2> Book Author Name : ${element.AuthorName}</h2> <h2> Book Category : ${element.Category}</h2>  <h2> Book Quantity :${element.Quantity}</h2> `;
-
+                bool= true;
+            }
+            else
+            {
+                alert("Out of stock");
+            }
+            break;
         }
-
-        if (element.Quantity > 0) {
-            element.Quantity--;
-        }
-        else {
-            console.log("Out of Stock");
-        }
-        break;
     }
     
-
+    if(!bool)
+    {
+        alert("Book is not avable");
+    }
 }
 
 
 document.querySelector('.return').onclick = function () {
     var name = prompt("Enter the book Name : ");
-
+    var bool = false    
     for (const element of books) {
         if (element.BookName === name) {
             element.Quantity++;
 
             document.querySelector('div').innerHTML += `<h2>Book ID : ${element.BookId}</h2>  <h2>Book Name :  ${element.BookName}</h2> <h2> Book Author Name : ${element.AuthorName}</h2> <h2> Book Category : ${element.Category}</h2>  <h2> Book Quantity :${element.Quantity}</h2> `;
-
+            bool=true;
 
         }
 
     }
-
+    if(!bool)
+    {
+        alert("Book is not avable");
+    }
 }
 
 document.querySelector('.delet').onclick = function ()
 {
    var deletname = prompt("Enter the book name you went to delet");
+    var bool = false    
    
     for (let i = 0; i < books.length; i++) {
         if (books[i].BookName === deletname) {
            books.splice(i,1);
             document.querySelector('div').innerHTML += `<h2>Book ID : ${element.BookId}</h2>  <h2>Book Name :  ${element.BookName}</h2> <h2> Book Author Name : ${element.AuthorName}</h2> <h2> Book Category : ${element.Category}</h2>  <h2> Book Quantity :${element.Quantity}</h2> `;
-
+            bool=true;
         }
 
     }
-   
+   if(!bool)
+    {
+        alert("Book is not avable");
+    }
 }
 document.querySelector('.status').onclick = function()
 {
     var bookstatus = prompt("Enter Book Name");
+    var bool = false    
+
     for (const element of books) 
     {
         if(element.BookName === bookstatus)
         {
            alert("Book is Available");
+           bool=true;
             
         }
         else
@@ -87,7 +103,10 @@ document.querySelector('.status').onclick = function()
         }
         break;
     }
-
+ if(!bool)
+    {
+        alert("Book is not avable");
+    }
 }
 
 document.querySelector('.total').onclick = function () {
